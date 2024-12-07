dd2763fd086e852a4da215abf3f7fcc2e86da7db
MuktaSingh1506 <90955197+MuktaSingh1506@users.noreply.github.com>
2024-12-07 16:59:04 +0000

    memory allocation in js notes

---
+++ 01_basics/memory_inJS.js
@@ -0,0 +1,17 @@
+/*
+Memory alloction in JS 
+type of memory:
+1>stack memory
+2>heap memory
+
+primitive datatypes use the STACK memory
+NON-primitive datatypes use the HEAP memory
+
+STACK - a copy of the data in the variable is used, 
+change in copy of the value not the original data.
+  
+HEAP - refernce of where the data is stored in memory is used ,
+means change in original value.
+
+
+*/
