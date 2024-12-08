6ef6561352fb4ebdc3faae01bc72f9b0d4ae0adf
MuktaSingh1506 <90955197+MuktaSingh1506@users.noreply.github.com>
2024-12-08 16:52:30 +0000

    string in js

---
+++ 01_basics/Strings_in_JS.js
@@ -0,0 +1,16 @@
+const name = "jane"
+const repCount = 10
+//battics syntax - string intercolation
+console.log('Hello my name is ${name} and my rep count is ${repcount}')
+
+// define varibale strng type usinf "new" keyword
+const gameName = new String("jane doe")
+console.log(typeof gameName) // object with key:value pair
+
+//string methods
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName)
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf("e"))
+
--- 01_basics/memory_inJS.js
+++ 01_basics/memory_inJS.js
@@ -11,7 +11,20 @@ STACK - a copy of the data in the variable is used,
 change in copy of the value not the original data.
   
 HEAP - refernce of where the data is stored in memory is used ,
-means change in original value.
-
-
+means change in original value
 */
+//stack - copy of data - no change to original data
+let name = "john doe"
+let name2 = name
+name2 = "jane doe"
+console.log(name)
+console.log(name2)
+
+//heap - refereance - original data change
+let user_one = {
+    email: "user_one@google.com",
+    upi: "user_one@ybl"
+}
+let user_two = user_one
+user_two.upi = "user_two@ybl"
+console.log(user_two.upi)
