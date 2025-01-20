cadd9f53696da80beb6a8cae1362da9fe32115bd
MuktaSingh1506 <90955197+MuktaSingh1506@users.noreply.github.com>
2025-01-20 16:46:23 +0000

    basic over and control statemet start

---
+++ 04_control_flow/one.js
@@ -0,0 +1,22 @@
+ /** conditional statement
+  * code should run on condition
+  * control flow or logic flow
+  */
+ if(false){
+// cause condtion is false this code block isn ot executed
+ }
+ if(true /*condition*/){
+// this code block will be excuted as condition is true
+ }
+
+// comparision
+if(2==2){
+    //
+}
+// or condtiton
+const isUserloggedin = true
+
+/**the condtions in control statedmet
+ * < > <= >= == !=
+ * 3!=2 - ?true 
+ */
