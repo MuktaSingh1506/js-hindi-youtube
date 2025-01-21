fa44b345022ffdd7ea73a33f7e652e25c85afbbe
MuktaSingh1506 <90955197+MuktaSingh1506@users.noreply.github.com>
2025-01-21 12:55:17 +0000

    conditional statement

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -15,8 +15,53 @@ if(2==2){
 }
 // or condtiton
 const isUserloggedin = true
-
+ if( isUserloggedin){
+    //will excute this block of code
+ }
 /**the condtions in control statedmet
- * < > <= >= == !=
- * 3!=2 - ?true 
+ * < ,> , <=,  >=,  == , !=, ===, !== //COMPARISON  operator
+ * 3!=2 - ?true as 3 is not equal to 2 (negative checking)
+ * = - assignmetnt operator
+ * == - compare two operators
+ * === - triple qual/ strict equal : checkss the opreators datatypes
+ * 
  */
+if( 2 == "2"){
+    console.log("yes with ==")
+}
+if( 2 === "2"){
+    console.log("yes ===")
+}
+if( "2" === "2"){
+    console.log("yes with ===")
+}
+
+let temp = 98.0
+if(temp === 40 ){
+    console.log("less thean 50")
+}else{
+    console.log( " temp greater than 50")
+}
+
+
+// scope related 
+const score = 200 
+if(score > 100 ){
+    /**the variable defined with in this {} 
+     * with be used only within this scope 
+     * var - keyword for varibale have global variable
+     * insted use let ,const to declare the variable
+     */
+    const power = 'fly'
+    console.log(`user power: ${power}`)
+}
+// console.log(`user power: ${power}`) // scope realated issue
+
+//shorthand notation for if condtion
+const balance = 1000
+if( balance > 500) console.log("test");
+/* this shorthand have inflicit scope meaning only one code line is written */
+
+
+// NESTING condtion
+
